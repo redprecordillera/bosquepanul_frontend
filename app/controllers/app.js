129af55731a282f18ad5a5019c67bee6be8b4e0f
mcueto <cueto@live.cl>
2017-04-12 12:53:58 +0200

    Add local images for birds

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -8,67 +8,67 @@ function AppController($scope){
       "scientific_name": "Leptasthenura aegithaloides",
       "vulnerability": "Fuera de Peligro",
       "description": "Tiene una longitud de 16 cm. Se alimenta de insectos, larvas y otros invertebrados terrestres. Anida desde septiembre con dos puestas al año. El nido, tipo taza, lo ubica en oquedades de árboles, techos de casas o muros y está fabricado de palos, ramitas u otras fibras vegetales e interiormente forrado de plumas. Es muy activa.",
-      "image_url": "http://bosquepanul.cl/images/ave_01.png?crc=4157276907"
+      "image_url": "media/images/birds/01.png"
     },{
       "name": "tenca",
       "scientific_name": "Mimus thenca",
       "vulnerability": "Fuera de Peligro",
       "description": "Posee un hermoso y variado canto, es conocida por imitar a otras aves. Acostumbra a pararse en las ramas más altas o en la misma punta de los árboles y arbustos. Posee una longitud de unos 28 cm de largo. Similar al zorzal en cuanto a tamaño, la tenca es más delgada y con la cola más larga.",
-      "image_url": "http://bosquepanul.cl/images/ave_02.png?crc=3823100294"
+      "image_url": "media/images/birds/02.png"
     },{
       "name": "pequén",
       "scientific_name": "Athene cunicularia",
       "vulnerability": "Fuera de Peligro",
       "description": "Es un búho pequeño de unos 23 cm de largo. Tiene ojos amarillos, y cejas claras. El dorso es castaño oscuro moteado de blanco. Tiene hábitos terrestres, anida en madrigueras y caza planeando, a menudo de día. Se alimenta de reptiles pequeños e insectos.",
-      "image_url": "http://bosquepanul.cl/images/ave_03.png?crc=14849829"
+      "image_url": "media/images/birds/03.png"
     },{
       "name": "Chercán",
       "scientific_name": "Troglodytes aedon",
       "vulnerability": "Fuera de Peligro",
       "description": "Es inconfundible por su pequeño tamaño, su color es café claro y la colita a veces parada. Tiene un largo total de 12 cm y ambos sexos no se diferencian en el color del plumaje. Vuela poco y a baja altura. Es muy inquieto y se mueve entre el ramaje de los arbustos, donde anda solo o en pareja.",
-      "image_url": "http://bosquepanul.cl/images/ave_04.png?crc=382438859"
+      "image_url": "media/images/birds/04.png"
     },{
       "name": "Perdiz",
       "scientific_name": "Nothoprocta perdicaria",
       "vulnerability": "Fuera de Peligro",
       "description": "Tiene un cuerpo compacto y casi sin cola, es fácil de identificar por su estridente reclamo al arrancar volando, asustando generalmente al visitante. Vive entre matorrales y pastizales abiertos. Está presente en gran parte de los terrenos del Bosque Panul.",
-      "image_url": "http://bosquepanul.cl/images/ave_05.png?crc=485888034"
+      "image_url": "media/images/birds/05.png"
     },{
       "name": "Loica",
       "scientific_name": "Sturnella loyca",
       "vulnerability": "Fuera de Peligro",
       "description": "Habita principalmente a ras de suelo en terrenos bajos y húmedos. Mide entre 26 y 28 cm. Busca su alimento al nivel del suelo, siendo las semillas y frutas su principal dieta. Suele andar en bandadas durante el invierno. Pone entre 3 y 4 huevos. El pecho rojo es característico del macho.",
-      "image_url": "http://bosquepanul.cl/images/ave_06.png?crc=4086118248"
+      "image_url": "media/images/birds/06.png"
     },{
       "name": "Tordo",
       "scientific_name": "Curaeus curaeus",
       "vulnerability": "Fuera de Peligro",
       "description": "Es de fácil identificación por ser completamente negro brillante, nidifica entre octubre y diciembre. Suele desplazarse en bandadas y son muy bulliciosos al comunicarse entre si. Se alimenta de semillas, frutos y pequeños invertebrados.",
-      "image_url": "http://bosquepanul.cl/images/ave_07.png?crc=236413169"
+      "image_url": "media/images/birds/07.png"
     },{
       "name": "Águila mora",
       "scientific_name": "Geranoaetus melanoleucus",
       "vulnerability": "Fuera de Peligro",
       "description": "Gran rapaz, fácil de identificar en vuelo por su forma triangular. Se alimenta de conejos, liebres, ratones, culebras y hasta zorros. Suele posarse en partes altas o sobrevolar largo rato una misma zona. A menudo se puede observar en laderas de las montañas y en los bordes del bosque, a veces se ve posada en las torres de alta tensión que cruzan el bosque. Mide entre 70 y 90 cm.",
-      "image_url": "http://bosquepanul.cl/images/ave_08.png?crc=4107641897"
+      "image_url": "media/images/birds/08.png"
     },{
       "name": "Picaflor chico",
       "scientific_name": "Sephanoides sephaniodes",
       "vulnerability": "Fuera de Peligro",
       "description": "Es la segunda ave más pequeña de Chile con tan sólo 11 cms. Anida durante el verano, armando el nido en ramas altas o arbustos. Coloca 2 huevos de color blanco opaco durante los meses de octubre y noviembre. Se alimenta de insectos y especialmente néctar de flores.",
-      "image_url": "http://bosquepanul.cl/images/ave_09.png?crc=3770829752"
+      "image_url": "media/images/birds/09.png"
     },{
       "name": "Cachudito",
       "scientific_name": "Anairetes parulus",
       "vulnerability": "Fuera de Peligro",
       "description": "Es una de las aves más pequeñas del país. Su principal característica son sus plumas oscuras y largas en la cabeza, dobladas hacia adelante. Tiene un largo de 11 cm. Muestra movimiento rápidos y ágiles.  Suele encontrarse en pequeñas bandadas, separándose durante la época de nidificación, se alimenta de insectos y semillas.",
-      "image_url": "http://bosquepanul.cl/images/ave_010.png?crc=3901814263"
+      "image_url": "media/images/birds/10.png"
     },{
       "name": "Picaflor cordillerano",
       "scientific_name": "Oreotrochilus leucopleurus",
       "vulnerability": "Vulnerable",
       "description": "Se le encuentra exclusivamente en la Cordillera de los Andes y hasta el borde de nieves eternas de verano. Anida en las salientes rocosas verticales de las laderas cercanas a los riachuelos cordilleranos, construyéndolo entre Noviembre y Diciembre. Va adherido a esta pared rocosa con un líquido pegajoso hecho con el néctar de las flores o la clara de un huevo. Mide 13 cm. de largo.",
-      "image_url": "http://bosquepanul.cl/images/ave_011.png?crc=3889604834"
+      "image_url": "media/images/birds/11.png"
     }
   ];
 
