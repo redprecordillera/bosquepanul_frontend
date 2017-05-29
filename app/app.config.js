014855b7b346489df9fd87421b6ec8e0c57b7e13
mcueto <cueto@live.cl>
2017-05-29 08:08:15 -0400

    Add production configurations

--- app/app.config.js
+++ app/app.config.js
@@ -1,31 +1,9 @@
-config.$inject = ['$stateProvider', '$urlRouterProvider', '$routeProvider'];
+config.$inject = ['$stateProvider', '$urlRouterProvider', '$routeProvider', '$compileProvider'];
 
-function config($stateProvider, $urlRouterProvider, $routeProvider){
-  $routeProvider
-  .when('/', {
-    templateUrl : 'app/templates/index.html'
-  })
-  .when('/bosque', {
-    templateUrl : 'app/templates/forest.html'
-  })
-  .when('/beneficios', {
-    templateUrl : 'app/templates/benefits.html'
-  })
-  .when('/flora_y_fauna', {
-    templateUrl : 'app/templates/flora.html'
-  })
-  .when('/ubicacion', {
-    templateUrl : 'app/templates/placement.html'
-  })
-  .when('/recomendaciones', {
-    templateUrl : 'app/templates/recommendations.html'
-  })
-  .when('/compartir', {
-    templateUrl : 'app/templates/share.html'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+function config($stateProvider, $urlRouterProvider, $routeProvider, $compileProvider){
+  $compileProvider.debugInfoEnabled(false);
+  $compileProvider.commentDirectivesEnabled(false);
+  $compileProvider.cssClassDirectivesEnabled(false);
 }
 
 export default config;
