62f15031d9c04d8face6dc0d4c6dea3892e91bb5
mcueto <cueto@live.cl>
2017-06-04 14:00:28 -0400

    Add wait to gifplayer instances to animate only when gif is full loaded

--- app/directives/gifplayer.js
+++ app/directives/gifplayer.js
@@ -5,7 +5,7 @@ function GifPlayerDirective(){
       restrict: 'A',
       link: function(scope, element, attrs) {
           element.bind('load', function() {
-              $(element).gifplayer({keepSize:false, customClass:'u-max-full-width'});
+              $(element).gifplayer({keepSize:false, customClass:'u-max-full-width', wait:true});
           });
           element.bind('error', function(){
               console.warn('image could not be loaded');
